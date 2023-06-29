b56da2a46216ae189b1f0262048c5a8486919351
D4v1D0n3 <d.doneddu@campus.unimib.it>
2023-06-29 11:29:47 +0200

    Primo commit

---
+++ project/database/src/main/java/it/unimib/finalproject/database/Prenotazione.java
@@ -0,0 +1,9 @@
+package it.unimib.finalproject.database;
+
+import java.util.List;
+
+public class Prenotazione {
+    private int id;
+    private List<String> posti;
+    private Proiezione proiezione;
+}
---
+++ project/database/src/main/java/it/unimib/finalproject/database/Proiezione.java
@@ -0,0 +1,12 @@
+package it.unimib.finalproject.database;
+
+import java.util.Hashtable;
+
+public class Proiezione {
+    private String[][] postiDisponibili;
+    private String descrizione;
+    private String titolo;
+    private String data;
+    private String ora;
+    
+}
